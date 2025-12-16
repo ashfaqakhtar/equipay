051f85c981a14b9cdc48c44cd3f45351c1548815
Ashfaq Akhtar <ashfaqakhtar@live.com>
2025-12-16 14:13:40 +0530

    forward shalsh in the url is removed and with of the website is reduced to 70rem

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // globalNotFound: true,
+  globalNotFound: true,
+  trailingSlash: false,
   output: "export",
   images: {
     unoptimized: true,
